a9db921d239ab5023c3ddb04efd9a358f53586e6
leedd87 <leedav87@gmail.com>
2022-09-23 16:03:43 -0300

    localstorage

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -4,6 +4,7 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const Tarea = ({ id, texto, completada, completarTarea, eliminarTarea }) => {
 	console.log("Hello");
+	console.log("Agregar LocalStorage");
 	return (
 		<div
 			className={
